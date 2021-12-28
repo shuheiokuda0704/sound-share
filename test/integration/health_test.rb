fdd9effbe4938a3175a309b7df59338bd210c357
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2021-12-28 17:44:27 +0900

    Add heath check endpoint

--- test/integration/health_test.rb
+++ test/integration/health_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class HealthTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can get health check result" do
+    get "/api/v1/health"
+    assert_response :success
+  end
 end
